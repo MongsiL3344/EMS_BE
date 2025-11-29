718cab408739725759b28a41831bed63e1270bf2
turt1e18 <pk0820@cu.ac.kr>
2025-11-29 20:28:03 +0900

    resend + dns 등록

--- src/signup/Signup.EmailCode.ts
+++ src/signup/Signup.EmailCode.ts
@@ -2,13 +2,24 @@ import crypto from 'crypto';
 import 'dotenv/config';
 import type { Request, Response } from 'express';
 import nodemailer from 'nodemailer';
+import { Resend } from 'resend';
 
+/**
+ * gmail == env
+ *
+ * @param req email
+ * @param res emailcode
+ */
 export async function sendCode(req: Request, res: Response) {
+  const resend = new Resend(process.env.RESEND_API);
+  resend.domains.create({ name: 'ems.sendmail.com' });
+
   try {
     const code = generateSecureCode();
-    const mailOptions = {
-      from: process.env.GMAIL_USER,
-      to: req.body.email,
+    //resend code
+    const { data, error } = await resend.emails.send({
+      from: 'turt1e18@turt1e18.work',
+      to: [req.body.email],
       subject: 'Your Verification Code',
       html: `
         <div style="font-family: Arial, sans-serif; padding: 20px;">
@@ -19,13 +30,38 @@ export async function sendCode(req: Request, res: Response) {
           <p>If you didn't request this code, please ignore this email.</p>
         </div>
       `,
-    };
-    await transporter.sendMail(mailOptions).then(() => {
-      res.status(200).json({
-        success: true,
-        code: code,
-      });
     });
+
+    if (error) {
+      return res.status(400).json({ error });
+    }
+
+    res.status(200).json({
+      success: true,
+      code: code,
+    });
+
+    //gamil code
+    // const mailOptions = {
+    //   from: process.env.GMAIL_USER,
+    //   to: req.body.email,
+    //   subject: 'Your Verification Code',
+    //   html: `
+    //     <div style="font-family: Arial, sans-serif; padding: 20px;">
+    //       <h2>Verification Code</h2>
+    //       <p>Your verification code is:</p>
+    //       <h1 style="color: #4CAF50; letter-spacing: 5px;">${code}</h1>
+    //       <p>This code will expire in 10 minutes.</p>
+    //       <p>If you didn't request this code, please ignore this email.</p>
+    //     </div>
+    //   `,
+    // };
+    // await transporter.sendMail(mailOptions).then(() => {
+    //   res.status(200).json({
+    //     success: true,
+    //     code: code,
+    //   });
+    // });
   } catch (err) {
     console.error('Send code error:', err);
     res.status(500).json({
